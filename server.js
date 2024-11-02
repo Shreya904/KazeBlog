b40503044c767d1a416e44f3d09ce490891491b3
Shreya904 <shreyasrivastava92006@gmail.com>
2024-11-02 14:20:36 +0530

    fixing bugs

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const jsonServer = require("json-server");
+import jsonServer from "json-server";
+import { createServer } from "http";
+
 const server = jsonServer.create();
-const router = jsonServer.router("data/db.json"); // Path to your JSON file
+const router = jsonServer.router("./data/db.json"); // Path to your JSON file
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+createServer(server).listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
 });
