ca9de66299d1dcd481435ffa428403ed5bbbdc74
Shreya904 <shreyasrivastava92006@gmail.com>
2025-01-28 16:03:58 +0530

    debugging

--- src/utils/appwrite.js
+++ src/utils/appwrite.js
@@ -3,8 +3,8 @@ import { Client, Databases } from "appwrite";
 export const client = new Client();
 
 client
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("67986711003bad2bc75c"); // Replace with your project ID
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT) // For Vite
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID); // For Vite
 
 export const database = new Databases(client);
 export { ID } from "appwrite";
